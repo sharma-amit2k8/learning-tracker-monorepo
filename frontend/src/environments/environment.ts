a1431e5c1c9e7df26dcf41581b4ca5cab584da0b
Amit <sharma.amit2k8@gmail.com>
2026-06-08 03:32:07 +0530

    updated backend URL to render url

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production : false,
-    baseURL : 'http://localhost:1200'
+    // baseURL : 'http://localhost:1200'
+    baseURL : 'https://learning-tracker-monorepo.onrender.com' 
 }
