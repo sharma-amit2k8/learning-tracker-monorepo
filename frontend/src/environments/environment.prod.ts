5a94e84291df69d74232ca2d60e08f6aadf37d8a
Amit <sharma.amit2k8@gmail.com>
2026-06-08 16:05:45 +0530

    updated baseURL for prod

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  baseURL : 'https://learning-tracker-monorepo.onrender.com' 
+};
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
     production : false,
-    // baseURL : 'http://localhost:1200'
-    baseURL : 'https://learning-tracker-monorepo.onrender.com' 
+    baseURL : 'http://localhost:1200'
 }
